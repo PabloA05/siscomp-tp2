7a8974215aaa4ad63570b7c5a3288c1fb91d2b6e
Cristian-DP <cristian.pereyra@mi.unc.edu.ar>
2023-04-06 22:32:16 -0300

    Esquelto para llamar al codigo asm

--- conversor.c
+++ conversor.c
@@ -4,23 +4,20 @@
 #define ARGCANT 0
 #define ARGTYPE 1
 #define ARGINVA 2
+#define RETERRO -1
 
-void _printbanner (float num1, float num2){
+void _printbanner (float num1){
     printf ("###############################################\n");
     printf ("----> Estamos en el wrapper conversor (C) <----\n");
-    printf ("Conversor: %f %f\n", num1, num2);
+    printf ("Conversor: %f \n", num1);
     printf ("###############################################\n");
 }
-
 
 void _printBannerError (int opt){
     printf ("###############################################\n");
     printf ("----> Estamos en el wrapper conversor (C) <----\n");
     switch (opt)
     {
-    case ARGCANT:
-        printf ("La cantidad de argumentos es invalida\n");
-        break;
     case ARGINVA:
         printf ("El valor del argumento es invalida\n");
         break;
@@ -28,17 +25,23 @@ void _printBannerError (int opt){
         break;
     }
     printf ("###############################################\n");
-
 }
 
 float conversor(float number1, float number2) {
 
+    printf ("--> Entrando a convesor ubicado en .c\n");
+    
+    // se comprueba los valores recibidos
     if (number1 < 0.0 || number2 < 0.0)
     {
         _printBannerError (ARGINVA);
-        return -1;
+        return RETERRO;
     }
     
-    _printbanner (number1, number2);
-    return 1000;
+    // se llama al codigo asm    
+    printf ("--> call <file>.asm\n");
+    float ret = 10.4; // = <file>.asm
+    printf ("--> Valor recibido de <file>.asm : %f \n", ret);
+
+    return ret;
 }
