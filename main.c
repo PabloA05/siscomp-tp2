1104c48b419c352a6534c9dc6a5723dc6ed29d6f
PabloA05 <pablo.cazon@alumnos.unc.edu.ar>
2023-04-08 11:45:41 -0300

    double -> float

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include "cdecl.h"
 
-double PRE_CDECL multiply( double, double ) POST_CDECL;
+float PRE_CDECL multiply( float, float ) POST_CDECL;
 
 int main()
 {
-  double d1, d2;
+  float d1, d2;
 
   printf("Enter two floats: ");
-  scanf("%lf %lf", &d1, &d2);
+  scanf("%f %f", &d1, &d2);
 
-  printf("Result of %g * %g is %g\n", d1, d2, multiply(d1,d2));
+  printf("Result of %f * %f is %f\n", d1, d2, multiply(d1,d2));
   return 0;
 }
--- multiply.asm
+++ multiply.asm
@@ -1,4 +1,3 @@
-;
 ; file: multiply.asm
 
 global multiply
@@ -6,13 +5,13 @@ global multiply
 segment .text
 
 %define d1   [ebp+8]
-%define d2   [ebp+16]
+%define d2   [ebp+12]
 
 multiply:
         enter   0, 0
 
-        fld     qword d2
-        fld     qword d1            ; ST0 = d1, ST1 = d2
+        fld     dword d2
+        fld     dword d1            ; ST0 = d1, ST1 = d2
         fmul          
 
 exit:
